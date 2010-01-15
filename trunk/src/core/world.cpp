f1e993e9e29e60b934e9cde023465fdd4de25ad5
henine <henine@ed12c444-3f30-488e-b1aa-86d1639ddb0b>
2010-01-15 13:23:27 +0000

    Stuff.

---
+++ trunk/src/core/world.cpp
@@ -0,0 +1,10 @@
+#include <core/world.h>
+
+World::World(int Height, int Width){
+	terrainHeight = Height;
+	terrainWidth = Width;
+}
+
+World::~World(){
+
+}
---
+++ trunk/src/gui/eventer.h
@@ -0,0 +1,7 @@
+/* Takes care of both ingame and IO events */
+
+#ifndef EVENTER_H_
+#define EVENTER_H_
+
+
+#endif /* EVENTER_H_ */
